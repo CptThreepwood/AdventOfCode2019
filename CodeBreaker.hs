a4fee8467887443e9d528d16462aa567fc989c71
David Jennens <d.jennens@gmail.com>
2019-12-22 00:58:23 +1100

    Give up on parser approach and use Rishi's group cheat.

--- CodeBreaker.hs
+++ CodeBreaker.hs
@@ -1,3 +1,5 @@
+import Data.List (group)
+
 -- Check that tuple elements are equal
 areEqual :: (Eq a) => (a, a) -> Bool
 areEqual (x, y) = x == y
@@ -6,9 +8,17 @@ areEqual (x, y) = x == y
 notDecreasing :: (Ord a) => (a, a) -> Bool
 notDecreasing (x, y) = x <= y
 
+-- Need at least one pair of adjacent digits equal with no more than that pair
+hasEqualPair :: (Eq a) => [a] -> Bool
+hasEqualPair x = or $ map ((==2) . length) $ group x
+
 -- Potentially loops through the list twice, but it's cleaner looking
 meetsCriteria :: (Ord a, Eq a) => [(a, a)] -> Bool
 meetsCriteria list = (or $ map areEqual list) && (and $ map notDecreasing list)
+
+-- Harsher criteria
+meetsHarsherCriteria :: (Ord a, Eq a) => [a] -> Bool
+meetsHarsherCriteria list = (hasEqualPair list) && (and $ map notDecreasing $ tuplise list)
 
 -- Create tuples out of pairs of elements in list
 tuplise :: [a] -> [(a, a)]
@@ -26,4 +36,15 @@ main = do
     -- print $ (meetsCriteria . tuplise $ "223450") == False
     -- print $ (meetsCriteria . tuplise $ "123789") == False
     -- Answer 1
-    print $ length . validCodes $ [168630..718098]
+    -- print $ length . validCodes $ [168630..718098]
+    -- Test 2
+    print $ (meetsHarsherCriteria $ "122345") == True
+    print $ (meetsHarsherCriteria $ "111123") == False
+    print $ (meetsHarsherCriteria $ "135679") == False
+    print $ (meetsHarsherCriteria $ "111111") == False
+    print $ (meetsHarsherCriteria $ "223450") == False
+    print $ (meetsHarsherCriteria $ "123789") == False
+    print $ (meetsHarsherCriteria $ "112233") == True
+    print $ (meetsHarsherCriteria $ "123444") == False
+    print $ (meetsHarsherCriteria $ "111122") == True
+    -- Answer 2
