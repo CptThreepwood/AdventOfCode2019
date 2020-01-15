a3d3de288b651542e879510bf9a8f87d4ac71232
David Jennens <d.jennens@gmail.com>
2020-01-15 16:32:02 +1100

    Part 1 complete

--- Day_7/ChainableIntcode.hs
+++ Day_7/ChainableIntcode.hs
@@ -164,7 +164,18 @@ runAmps :: Tape -> Settings -> IO Int
 runAmps program settings = foldM (\acc x -> runAmp program (V.fromList [x, acc])) 0 settings
 
 main = do
-    -- content <- readFile "input_5.txt"
-    let program = string2Tape "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
-    a <- mapM (runAmps program) $ Data.List.permutations [0..4]
-    print $ maximum a
+    -- Tests
+    -- let program = string2Tape "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+    -- thrusts <- mapM (runAmps program) $ Data.List.permutations [0..4]
+    -- print $ (maximum thrusts) == 43210
+    -- let program = string2Tape "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+    -- thrusts <- mapM (runAmps program) $ Data.List.permutations [0..4]
+    -- print $ (maximum thrusts) == 54321
+    -- let program = string2Tape "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+    -- thrusts <- mapM (runAmps program) $ Data.List.permutations [0..4]
+    -- print $ (maximum thrusts) == 65210
+
+    -- Part 1
+    program <- string2Tape <$> readFile "input_7.txt"
+    thrusts <- mapM (runAmps program) $ Data.List.permutations [0..4]
+    print $ (maximum thrusts)
